c7c73704a2d34ab8bb39a7e7879e24eafd11bf4f
Raza Aziz <razaaziz9191@gmail.com>
2026-01-06 01:04:33 +0500

    Built Blog Post Card and Dropdown components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,19 @@ import Footer from "./Footer/Footer";
 import LogOutBtn from "./Header/LogOutBtn";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Button from "./Button";
 
-export { Header, Footer, LogOutBtn, Container, Logo };
+export {
+  Header,
+  Footer,
+  LogOutBtn,
+  Container,
+  Logo,
+  Login,
+  PostCard,
+  Button,
+  Select,
+};
